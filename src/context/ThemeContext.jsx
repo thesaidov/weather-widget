af2c561bd23a17b4e2e37802b13525c92895cb95
Sardorbek Saidov <sardorbekkamoliddinovich@gmail.com>
2025-07-21 13:31:49 +0500

    changed themeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -13,7 +13,7 @@ const ThemeProvider = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div className={`${theme ? "bg-[#f8f9fa] text-[#212529] accent-[#0d6efd]" : "bg-[#212529] text-[#f8f9fa] accent-[#0d6efd]"} px-16 pt-10`}>{children}</div>
+      <div className={`${theme ? "bg-[#f8f9fa] text-[#212529] accent-[#0d6efd]" : "bg-[#212529] text-[#f8f9fa] accent-[#0d6efd]"} px-16 max-[400px]:px-3 pt-10`}>{children}</div>
     </ThemeContext.Provider>
   );
 };
